988db0d18c26aaa45db2aba0a7af4c07d78c2d1e
Xu Zhao <xzhao9@meta.com>
2025-11-12 15:32:06 -0800

    [install] fix helion install (#640)

--- tools/helion/install.py
+++ tools/helion/install.py
@@ -8,8 +8,8 @@ from pathlib import Path
 REPO_PATH = Path(os.path.abspath(__file__)).parent.parent.parent
 CURRENT_DIR = Path(os.path.abspath(__file__)).parent
 HELION_INSTALL_PATH = REPO_PATH.joinpath(".install")
-HELION_REPO = "https://github.com/xuzhao9/helion.git"
-HEION_COMMIT = "xz9/ptxas-knobs"
+HELION_REPO = "https://github.com/pytorch/helion.git"
+HEION_COMMIT = "51580b43bd65978a28b6e5bcd6f625485f02cba1"
 
 
 def install_helion():
@@ -21,12 +21,5 @@ def install_helion():
     subprocess.check_call(git_clone_cmd, cwd=HELION_INSTALL_PATH)
     git_checkout_cmd = ["git", "checkout", HEION_COMMIT]
     subprocess.check_call(git_checkout_cmd, cwd=HELION_PATH)
-    install_requirements_cmd = [
-        "pip",
-        "install",
-        "-r",
-        "requirements.txt",
-    ]
-    subprocess.check_call(install_requirements_cmd, cwd=HELION_PATH)
     install_helion_cmd = ["pip", "install", "-e", ".[dev]"]
     subprocess.check_call(install_helion_cmd, cwd=HELION_PATH)
