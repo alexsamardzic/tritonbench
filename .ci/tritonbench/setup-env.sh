a77afbe38cdd1f2c1d193de0e46d1b81b0ad6a60
Xu Zhao <xzhao9@meta.com>
2026-01-14 17:47:12 -0500

    [ci] Add nightly testing for triton-meta (#775)

--- .ci/tritonbench/setup-env.sh
+++ .ci/tritonbench/setup-env.sh
@@ -15,6 +15,8 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         --cuda) USE_CUDA="1";  ;;
         --hip) USE_HIP="1"; ;;
+        --triton-main) USE_TRITON_MAIN="1"; ;;
+        --meta-triton) USE_META_TRITON="1"; ;;
         *) echo "Unknown parameter passed: $1"; usage ;;
     esac
     shift
@@ -24,21 +26,21 @@ if [ ! -e ${WORKSPACE_DIR} ]; then
     sudo mkdir -p ${WORKSPACE_DIR}
     sudo chown -R $(whoami):$(id -gn) ${WORKSPACE_DIR}
 fi
-touch "${SETUP_SCRIPT}"
 
+touch "${SETUP_SCRIPT}"
 echo ". ${SETUP_SCRIPT}" >> ${HOME}/.bashrc
-echo "if [ -z \${CONDA_ENV} ]; then export CONDA_ENV=${CONDA_ENV}; fi" >> "${SETUP_SCRIPT}"
 
 if [ -n "${UV_VENV_DIR:-}" ]; then
     bash ./.ci/uv/install.sh
     . $HOME/.local/bin/env 
 else
     bash ./.ci/conda/install.sh
+    . "${SETUP_SCRIPT}"
 fi
 
-. "${SETUP_SCRIPT}"
-
 export CONDA_ENV=pytorch
+echo "if [ -z \${CONDA_ENV} ]; then export CONDA_ENV=${CONDA_ENV}; fi" >> "${SETUP_SCRIPT}"
+
 python3 tools/python_utils.py --create-conda-env ${CONDA_ENV}
 if [ -n "${UV_VENV_DIR:-}" ]; then
     echo ". ${UV_VENV_DIR}/\${CONDA_ENV}/bin/activate" >> "${SETUP_SCRIPT}"
@@ -81,9 +83,11 @@ if [ -n "${USE_CUDA:-}" ]; then
     sudo apt-get purge -y '^nvidia-'
 fi
 
-CONDA_ENV_TRITON_MAIN=triton-main
-bash .ci/triton/install.sh --conda-env "${CONDA_ENV_TRITON_MAIN}" \
-        --repo triton-lang/triton --commit main --side single --nightly \
-        --install-dir ${WORKSPACE_DIR}/triton-main
+if [ -n "${USE_TRITON_MAIN:-}" ]; then
+    bash ./.ci/triton/install-triton-main.sh
+fi
+if [ -n "${USE_META_TRITON:-}" ]; then
+    bash ./.ci/triton/install-meta-triton.sh
+fi
 
 cat "${SETUP_SCRIPT}"
